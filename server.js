df41f20e0e155df0e59d6c77a05d5bcff4cb19af
AyaMu <ayalamuruch@gmail.com>
2023-01-12 18:52:24 -0500

    add error.ejs and index.ejs to views/meals directory

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
 import methodOverride from 'method-override'
+
 import './config/database.js'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
-
 import { router as flightsRouter } from './routes/flights.js'
+import { router as mealsRouter } from './routes/meals.js'
+
 
 // set up app
 const app = express()
@@ -37,6 +39,7 @@ app.use(methodOverride('_method'))
 
 app.use('/', indexRouter)
 app.use('/flights', flightsRouter)
+app.use('/meals', mealsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
